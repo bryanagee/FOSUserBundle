07798ae450db4db5b0b93d84e68e0cfa3bf41a9d
Bryan J. Agee <bryan@pamiris.com>
2014-11-10 14:40:30 -0800

    Adds docblocks to Canonicalizer

--- Util/Canonicalizer.php
+++ Util/Canonicalizer.php
@@ -11,10 +11,20 @@
 
 namespace FOS\UserBundle\Util;
 
+/**
+ * The Canonicalizer centralizes the task of normalizing strings for searching
+ * and indexing--especailly in case-sensitive datastores.
+ */
 class Canonicalizer implements CanonicalizerInterface
 {
+    /**
+     * 
+     * @param string $string
+     * @return string
+     */
     public function canonicalize($string)
     {
-        return null === $string ? null : mb_convert_case($string, MB_CASE_LOWER, mb_detect_encoding($string));
+        return null === $string ? null 
+                : mb_convert_case($string, MB_CASE_LOWER, mb_detect_encoding($string));
     }
 }
