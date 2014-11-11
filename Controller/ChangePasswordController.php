dfd8aa1dd41db66f2c90c842737ab9c5fcad202a
Bryan J. Agee <bryan@pamiris.com>
2014-11-11 00:51:32 -0800

    Removes spaces around a bang

--- Controller/ChangePasswordController.php
+++ Controller/ChangePasswordController.php
@@ -36,7 +36,7 @@ class ChangePasswordController extends Controller
     public function changePasswordAction(Request $request)
     {
         $user = $this->getUser();
-        if ( ! is_object($user) || ! $user instanceof UserInterface) {
+        if (!is_object($user) || ! $user instanceof UserInterface) {
             throw new AccessDeniedException('This user does not have access to this section.');
         }
 
